a3fbf1c0fcae447ce96e2fe59993ec980bb50cb5
MATVEN <mattiavena2993@gmail.com>
2019-11-07 10:17:24 +0100

    add js and jq function

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// cambiare colore caselle al click
+$(".red").click(function() {
+  $(this).css( "background-color", "red" );
+})
+
+$(".green").click(function() {
+  $(this).css( "background-color", "green" );
+})
